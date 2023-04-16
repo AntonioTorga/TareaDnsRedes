77170f9dcddc68565ca3902fdb1ac2822dc6524c
AntonioTorga <antonio.torga@ug.uchile.cl>
2023-04-16 15:50:46 -0400

    Terminada parte 2

--- resolver.py
+++ resolver.py
@@ -1,5 +1,30 @@
 import binascii
 import socket
+from dnslib import DNSRecord
+
+ip_root = "192.33.4.12"
+ip_port = 54
+
+def parse_raw_dns(message):
+    parsed = DNSRecord.parse(message)
+    ANCOUNT = parsed.header.a
+    NSCOUNT = parsed.header.auth
+    ARCOUNT = parsed.header.ar
+    Answer = parsed.rr
+    Authority = parsed.auth
+    Additional = parsed.ar
+    
+    struct = {"ANCOUNT": ANCOUNT,
+              "NSCOUNT": NSCOUNT,
+              "ARCOUNT": ARCOUNT,
+              "Answer": Answer,
+              "Authority": Authority,
+              "Additional": Additional,
+              }
+    return struct
+
+def resolver(mensaje_consulta):
+    pass
 
 buff_size = 2048
 socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -7,8 +32,12 @@ resolver_address = ("localhost", 8000)
 socket.bind(resolver_address)
 while(True):
     message,_ = socket.recvfrom(buff_size)
-    print(message)
+
+    struct = parse_raw_dns(message)
+    print(struct)
 
 
 
 
+
+#Guardar: Qname, ANCOUNT, NSCOUNT, ARCOUNT, Answer, Authority y Additional
