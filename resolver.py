2ea1ba9f98c51f7fb6bee3576f82a5230aa290e3
AntonioTorga <antonio.torga@ug.uchile.cl>
2023-04-17 19:11:43 -0400

    Lista parte 4

--- resolver.py
+++ resolver.py
@@ -5,6 +5,7 @@ from dnslib import DNSRecord
 
 def parse_raw_dns(message):
     parsed = DNSRecord.parse(message)
+    Qname = str(parsed.get_q().get_qname())
     ANCOUNT = parsed.header.a
     NSCOUNT = parsed.header.auth
     ARCOUNT = parsed.header.ar
@@ -12,7 +13,9 @@ def parse_raw_dns(message):
     Authority = parsed.auth
     Additional = parsed.ar
     
-    struct = {"ANCOUNT": ANCOUNT,
+    struct = {  
+              "Qname":Qname,
+              "ANCOUNT": ANCOUNT,
               "NSCOUNT": NSCOUNT,
               "ARCOUNT": ARCOUNT,
               "Answer": Answer,
@@ -21,23 +24,63 @@ def parse_raw_dns(message):
               }
     return struct
 
-def resolver(mensaje_consulta):
+def res_aux(mes,mensaje_consulta,debug=False):
+    new_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    parsed_mes = parse_raw_dns(mes)
+
+    if (parsed_mes["ANCOUNT"] > 0):
+        for item in parsed_mes["Answer"]:
+            if item.rtype==1:
+                return mes
+    if (parsed_mes["NSCOUNT"]>0):
+        for item in parsed_mes["Authority"]:
+            ns_detour=False
+            if item.rtype==2:
+                ns_detour = True
+                break
+        if ns_detour:
+            ip_found = False
+            for item in parsed_mes["Additional"]:
+                if item.rtype == 1:
+                    ip_found = True
+                    add = (str(item.rdata),53)
+                    if debug:
+                        print(f'(debug) Consultando "{parsed_mes["Qname"]}" a "{item.rname}" en "{item.rdata}"')
+                    new_sock.sendto(mensaje_consulta,add)
+                    response,_ = new_sock.recvfrom(buff_size)
+            if (not ip_found):
+                ns_name=""
+                for item in parsed_mes["Authority"]:
+                    if item.rtype == 2:
+                        ns_name = str(item.rdata)
+                        break
+                if ns_name!="":
+                    ns_raw_answer = resolver(bytes(DNSRecord.question(ns_name).pack()))
+                    ns_answer = parse_raw_dns(ns_raw_answer)
+                    ns_answer = ns_answer["Answer"]
+                    ns_ip = ""
+                    for item in ns_answer:
+                        if item.rtype == 1:
+                            ns_ip = str(item.rdata)
+                            break
+                    if ns_ip!="":
+                        if debug:
+                            print(f'(debug) Consultando "{parsed_mes["Qname"]}" a "{ns_name}" en "{ns_ip}"')
+                        new_sock.sendto(mensaje_consulta,(ns_ip,53))
+                        response,_ = new_sock.recvfrom(buff_size)
+        return response
+
+def resolver(mensaje_consulta,debug=False):
     new_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     new_sock.sendto(mensaje_consulta,root)
-    message,address = new_sock.recvfrom(buff_size)
-    parsed = parse_raw_dns(message)
-    if parsed["ANCOUNT"] > 0:
-        for record in parsed["Answer"]:
-            if record.rclass == 1:
-                return message
-    elif parsed["NSCOUNT"] > 0:
-        for record in parsed["Additional"]:
-            if record.rclass == 1:
-                pass #mandar message a records
-            
+    mes,address = new_sock.recvfrom(buff_size)
 
+    result = res_aux(mes,mensaje_consulta,debug)
 
- 
+    while(parse_raw_dns(result)["ANCOUNT"]==0):
+        result = res_aux(result,mensaje_consulta)
+    
+    return result
 
 
 root = ('192.33.4.12', 53)
@@ -48,12 +91,9 @@ resolver_socket.bind(resolver_address)
 
 
 while(True):
-    message,_ = resolver_socket.recvfrom(buff_size)
+    message,req_add = resolver_socket.recvfrom(buff_size)
+    
+    result = resolver(message,True)
 
-    resolver(message)
+    resolver_socket.sendto(result, req_add)
 
-
-
-
-
-#Guardar: Qname, ANCOUNT, NSCOUNT, ARCOUNT, Answer, Authority y Additional
